49c4ceb4e78634dd340701a2095ceabfc9a67805
Ignacio R. Morelle <shadowm@wesnoth.org>
2008-03-31 12:37:15 +0000

    * Various updates for 1.9.0
    [Wesnoth-UMC-Dev SVN r51]

--- bin/probe_tools/package-gettext.sh
+++
@@ -1,7 +0,0 @@
-#
-#   $Id$
-#
-if [ $(msgfmt --version | grep "gettext-tools") = "" ]; then
-	echo -e "PROBE: failed to detect gettext-tools; please install the \"gettext-tools\" package first!"
-	return -1
-fi
--- bin/probe_tools/package-gnused.sh
+++
@@ -1,7 +0,0 @@
-#
-#   $Id$
-#
-if [ $(sed --version | grep "GNU sed") = "" ]; then
-	echo -e "PROBE: failed to detect GNU sed; please install the \"GNU-sed\" package first!"
-	return -1
-fi
