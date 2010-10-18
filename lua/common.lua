c5d70a3b0af6fcdcaa95395b5fb8059277e5594a
Ignacio R. Morelle <shadowm@wesnoth.org>
2010-10-18 22:02:08 +0000

    Initial lua stuff
    [Wesnoth-UMC-Dev SVN r7695]

---
+++ lua/common.lua
@@ -0,0 +1,17 @@
+local helper = wesnoth.require "lua/helper.lua"
+local wml_actions = wesnoth.wml_actions
+
+function wesnoth.wml_actions.setup_doors(cfg)
+	local locs = wesnoth.get_locations {
+		terrain = "*^Z\\",
+		{ "or", { terrain = "*^Z/" } },
+	}
+
+	for k, loc in ipairs(locs) do
+		wesnoth.put_unit(loc[1], loc[2], {
+			type = "Door",
+			side = cfg.side,
+			id = string.format("__door_X%dY%d", loc[1], loc[2]),
+		})
+	end
+end
