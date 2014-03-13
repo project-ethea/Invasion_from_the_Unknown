6ef2bf04f55bf8d00d492659141274001936120a
Ignacio R. Morelle <shadowm@wesnoth.org>
2014-03-12 21:37:59 -0300

    Rename the internal safe_random() variable so it doesn't clash with AtS

--- lua/common.lua
+++ lua/common.lua
@@ -4,12 +4,12 @@
 
 function safe_random(arg)
 	wesnoth.fire("set_variable", {
-		name = "temp_ats_lua_random",
+		name = "temp_iftu_lua_random",
 		rand = arg,
 	})
 
-	local r = wesnoth.get_variable("temp_ats_lua_random")
-	wesnoth.set_variable("temp_ats_lua_random")
+	local r = wesnoth.get_variable("temp_iftu_lua_random")
+	wesnoth.set_variable("temp_iftu_lua_random")
 
 	return r
 end
