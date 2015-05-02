e269c81a4998fa957b46ce9da9d284296490a417
Ignacio R. Morelle <shadowm@wesnoth.org>
2015-05-02 03:51:39 -0300

    Add wput() and wprintf() wrappers for [wml_message] calls from Lua

--- lua/lib.lua
+++ lua/lib.lua
@@ -19,3 +19,55 @@ function safe_random(arg)
 
 	return r
 end
+
+---
+-- Log levels for wput and wprintf.
+---
+
+W_ERR  = 1 -- Error.
+W_WARN = 2 -- Warning (default maximum log level).
+W_INFO = 3 -- Info.
+W_DBG  = 4 -- Debug.
+
+local loglvl_map = { "error", "warning", "info", "debug" }
+
+---
+-- Prints a text line using the engine log facilities.
+--
+-- lvl: One of W_ERR, W_WARN, W_INFO, or W_DBG.
+-- msg: Line contents.
+---
+function wput(lvl, msg)
+	wesnoth.wml_actions.wml_message({
+		logger = loglvl_map[math.max(1, math.min(lvl, #loglvl_map))],
+		message = "[IftU] " .. msg
+	})
+end
+
+---
+-- Prints a formatted (printf-style) line using the engine log facilities.
+--
+-- lvl: One of W_ERR, W_WARN, W_INFO, or W_DBG.
+-- fmt: Line format specification.
+-- ...: Parameters for line formatting.
+---
+function wprintf(lvl, fmt, ...)
+	wput(lvl, string.format(fmt, ...))
+end
+
+---
+-- Returns a textdomain-specific string translated to the current locale
+--
+-- Note that this is only intended to be used with mainline textdomains. For
+-- add-on textdomains the _ hack including comments with fake WML is still
+-- needed so that wmlxgettext can catch and add the new strings.
+---
+function wgettext(str, domain)
+	if domain == nil then
+		domain = "wesnoth"
+	end
+
+	return wesnoth.textdomain(domain)(str)
+end
+
+wprintf(W_INFO, "Version %s initializing", PROJECT_Y_RECONSTRUCTION_VERSION)
