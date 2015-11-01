aeb3f8a161d22dc3cfa47d6b658c9d6dfba0c6e3
Ignacio R. Morelle <shadowm@wesnoth.org>
2015-10-31 22:12:35 -0300

    Remove Lua safe_random(), superseded by helper.rand()

--- lua/lib.lua
+++ lua/lib.lua
@@ -10,24 +10,6 @@ function split(s)
 end
 
 ---
--- Returns a pseudorandom value from a set.
---
--- This uses the same format and implementation as WML [set_variable], so that
--- the function is MP and replay-safe (unlike Lua's math.random).
----
-function safe_random(arg)
-	wesnoth.fire("set_variable", {
-		name = "temp_iftu_lua_random",
-		rand = arg,
-	})
-
-	local r = wesnoth.get_variable("temp_iftu_lua_random")
-	wesnoth.set_variable("temp_iftu_lua_random")
-
-	return r
-end
-
----
 -- Returns a pseudorandom value from a set without syncing replays/MP.
 --
 -- This will use the same format as WML [set_variable] in the future, maybe,
