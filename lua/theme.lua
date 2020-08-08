ee34e69f6f24debb868f98cf161561e290314a16
Iris Morelle <shadowm@wesnoth.org>
2020-08-08 00:42:59 -0400

    lua: Use register_unit_status_display()

--- lua/theme.lua
+++ lua/theme.lua
@@ -4,19 +4,9 @@
 
 -- #textdomain wesnoth-Invasion_from_the_Unknown
 local _ = wesnoth.textdomain "wesnoth-Invasion_from_the_Unknown"
-local old_unit_status = wesnoth.theme_items.unit_status
 
-function wesnoth.theme_items.unit_status()
-	local u = wesnoth.get_displayed_unit()
-	if not u then return {} end
-	local s = old_unit_status()
-
-	if u.status.dehydrated then
-		table.insert(s, { "element", {
-			image = "misc/dehydrated-status-icon.png",
-			tooltip = _ "dehydrated: This unit is dehydrated."
-		}})
-	end
-
-	return s
-end
+register_unit_status_display {
+	id      = "dehydrated",
+	icon    = "misc/dehydrated-status-icon.png",
+	tooltip = _ "dehydrated: This unit is dehydrated.",
+}
