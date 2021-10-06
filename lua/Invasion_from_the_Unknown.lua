1fc92208e92fe08fbc4b56e291403df72dc51607
Iris Morelle <shadowm@wesnoth.org>
2021-10-06 15:25:48 -0300

    Use wesnoth.units.find_on_recall

--- lua/Invasion_from_the_Unknown.lua
+++ lua/Invasion_from_the_Unknown.lua
@@ -98,7 +98,7 @@ end
 ----------
 
 function wesnoth.wml_actions.recall_all(cfg)
-	local units = wesnoth.get_recall_units(cfg)
+	local units = wesnoth.units.find_on_recall(cfg)
 
 	for i, u in ipairs(units) do
 		wesnoth.wml_actions.recall({ id = u.id })
