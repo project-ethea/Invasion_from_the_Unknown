a694869af0439e5a18a0fbff8e1719a565007c2d
Iris Morelle <shadowm@wesnoth.org>
2025-05-04 10:04:31 -0400

    Add taint status icons to the game sidebar

--- lua/theme.lua
+++ lua/theme.lua
@@ -5,8 +5,25 @@
 -- #textdomain wesnoth-Invasion_from_the_Unknown
 local _ = wesnoth.textdomain "wesnoth-Invasion_from_the_Unknown"
 
+local function register_taint_status_display(num, label)
+	register_unit_status_display {
+		id      = ("taint_%d"):format(num),
+		icon    = ("misc/tainted-status-icon-%d.png"):format(num),
+		tooltip = tostring( _ "%s: This unit is tainted by otherworldly corrosion."):format(tostring(label))
+	}
+end
+
+register_taint_status_display(1, "taint I")
+
+register_taint_status_display(2, "taint II")
+
+register_taint_status_display(3, "taint III")
+
+register_taint_status_display(4, "taint IV")
+
 register_unit_status_display {
 	id      = "dehydrated",
 	icon    = "misc/dehydrated-status-icon.png",
 	tooltip = _ "dehydrated: This unit is dehydrated.",
 }
+
